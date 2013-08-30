9e0540e25dc10e945b1098a69665dafa680e35e0
Stephen McGillen <smcgillen@hotmail.com>
2013-08-30 13:00:14 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecoin::Application.config.secret_token = 'c860884b48bd248702b9b45331e3da352ae07867f02b48b4429b72e84815e446b827ce9ad354eb107598077da997fc5740388b9887b1d3bc3be166b8ccc0a595'
