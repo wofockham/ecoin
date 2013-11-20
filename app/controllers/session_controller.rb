5df2efb1799df451df07371a1fa4a09a95e56357
Cold Head <certainthings@gmail.com>
2013-11-20 20:24:25 +1100

    session controller cleanup

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,22 +1,20 @@
 class SessionController < ApplicationController
-
   def new
   end
 
   def create
-  @merchant = Merchant.where(:email => params[:merchant][:email]).first
+    @merchant = Merchant.where(:email => params[:merchant][:email]).first
 
-  if @merchant.present? && @merchant.authenticate(params[:merchant][:password])
-
-  session[:merchant_id] = @merchant.id
-  redirect_to(sendtxt_path)
-  else
-  redirect_to(login_path)
-  end
+    if @merchant.present? && @merchant.authenticate(params[:merchant][:password])
+      session[:merchant_id] = @merchant.id
+      redirect_to(sendtxt_path)
+    else
+      redirect_to(login_path)
+    end
   end
 
   def destroy
-  session[:merchant_id] = nil
-  redirect_to(root_path)
+    session[:merchant_id] = nil
+    redirect_to(root_path)
   end
 end
